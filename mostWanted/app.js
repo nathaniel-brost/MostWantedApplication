6efd46ae522a93036432d8fe13ff4e89a18c2ee6
Nathaniel Brost <NathanielBrost@NathaniBook2021.attlocal.net>
2021-12-02 17:29:55 -0600

    pull down from repo

--- mostWanted/app.js
+++ mostWanted/app.js
@@ -12,8 +12,8 @@ function promptFor(question, valid){
   } while(!response || !valid(response));
   return response;
 }
-console.log(data[0].firstName);
-//promptFor(nameQ,chars);
+// console.log(data[0].firstName);
+// promptFor(nameQ,chars);
 
 
 // helper function to pass into promptFor to validate yes/no answers
@@ -23,6 +23,7 @@ function yesNo(input){
 
 // helper function to pass in as default promptFor validation
 function chars(input){
+  //code to validate, what will we accept
   return true; // default validation only
 }
 
@@ -57,25 +58,17 @@ function mainMenu(person, people){
     return app(people); // restart
   }
 
-  let displayOption = prompt("Found " + person[0].firstName + " " + person[0].lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want, 'restart', or 'quit'");
+  let displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want, 'restart', or 'quit'");
 
   switch(displayOption){
     case "info":
-      console.log(person[0])
     // TODO: get person's info
     break;
     case "family":
-      console.log(person[0].parent)
     // TODO: get person's family
     break;
     case "descendants":
-      //console.log()
     // TODO: get person's descendants
-    // Create a recursive function and what the function will do will create a variable w/ persons ID#, 
-    //filter out any object that doesn't have our persons ID in the parents field 
-    //at the end of the function, we'll call the function again for a new array ("kids array"), this will compare their ID#s to 
-    //all other objects in the data/first array.
-    //if array.lenght is >0 then we'll run the function. When there isn't grandkids/great grandkids it will stop the loop.
     break;
     case "restart":
     app(people); // restart
@@ -101,8 +94,8 @@ function searchByName(people){
   })
   // TODO: find the person using the name they entered
   return foundPerson;
-  
 }
+
 // alerts a list of people
 function displayPeople(people){
   alert(people.map(function(person){
